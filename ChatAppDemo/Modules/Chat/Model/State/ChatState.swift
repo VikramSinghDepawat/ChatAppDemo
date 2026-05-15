14a4fcccca262fef0fb915a6fecbbd2939853de3
vikramsingh <vc7662@gmail.com>
2026-05-15 13:42:54 +0530

    Create Chat State we use chat state for single source truth.

---
+++ ChatAppDemo/Modules/Chat/Model/State/ChatState.swift
@@ -0,0 +1,14 @@
+//
+//  ChatState.swift
+//  ChatAppDemo
+//
+//  Created by Vikram Singh Depawat on 15/05/26.
+//
+
+import Foundation
+
+struct ChatState {
+    var items: [ChatItem] = []
+    var isLoading: Bool = false
+    var isTyping: Bool = false
+}
